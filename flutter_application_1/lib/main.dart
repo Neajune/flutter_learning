730b5e0067cf6e565538f343139439323bd94e72
Neajune <corea2u2@gmail.com>
2024-03-10 09:38:43 +0900

    feat: Add a Total Balance (Text), white color

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -9,32 +9,54 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green.shade200 ,
-        body: const Column(
-          children: [
-            SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Column(
-                  children: [
-                  Text('Hey, Selena',
-                    style: TextStyle(
-                      color: Colors.white,
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 40,
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
                       ),
-                    ),
-                  Text('Welcome back',
-                    style: TextStyle(
-                      color: Colors.white,
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 0.8),
+                          fontSize: 18,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  )
+                ],
+              ), 
+              SizedBox(
+                height: 120,
+              ),
+              //Total Balance (Text), white color
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
                 ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ), 
         ),
       ),
     );
